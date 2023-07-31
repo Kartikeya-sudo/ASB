f667781474ced62665898cb2c37c44ec958472d3
Kartikeya Srigiriraju <kartikeya.srigiriraju@gmail.com>
2023-07-31 16:42:28 +0530

    Changed function location

--- main.c
+++ main.c
@@ -117,6 +117,29 @@ Url* parse_url(char* string) {
     return result;
 }
 
+void html_parser(char* response) {
+    char* pointer = response;
+
+    bool in_tag = false;
+    while (*pointer != '<') pointer++;
+
+    while (pointer != NULL && *pointer != '\0') {
+
+        if (*pointer == '<') {
+            in_tag = true;
+        } else if (*pointer == '>') {
+            in_tag = false;
+        } else {
+            if (in_tag == false) {
+                printf("%c", *pointer);
+            }
+        }
+
+        pointer++;
+    }
+}
+
+
 void http(Url* url) {
     int status, sockfd;
     struct addrinfo hints;
@@ -210,25 +233,6 @@ void http(Url* url) {
     close(sockfd);
 
     return ;
-}
-
-void html_parser(char* response) {
-    char* pointer = response;
-
-    bool in_tag = false;
-    while (*pointer != '<') pointer++;
-
-    while (pointer != NULL && *pointer != '\0') {
-        if (*pointer == '<') in_tag = true;
-
-        else if (*pointer == '>') in_tag = false;
-
-        else {
-            if (in_tag == false) printf("%c", *pointer);
-        }
-
-        pointer++;
-    }
 }
 
 void https(Url* url) {
